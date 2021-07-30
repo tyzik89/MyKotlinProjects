6245e902a6dda01dfb743ebe4a0cc7aa04f4118c
vlsu1118 <vladimir.sukmanov@netcracker.com>
2021-07-30 17:53:36 +0300

    Start to study kotlin

---
+++ Lessons/src/com.work.vladimirs/init/main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var x = 1
+    println("Before the loop: $x")
+
+    while (x < 4) {
+        println("In the loop: $x")
+        x++
+    }
+
+    println("After the loop: $x")
+}
