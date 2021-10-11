75c445222c7f1acac2d334f51303340ef1a0f3e1
Vladimir <work.vladimir.s@gmail.com>
2021-10-11 23:03:40 +0300

    New coroutines project

---
+++ DrumMachine/src/main/java/com/work/vladimirs/Beats.kt
@@ -0,0 +1,32 @@
+package com.work.vladimirs
+
+import java.io.File
+import javax.sound.sampled.AudioSystem
+
+fun main() {
+    playBeats("x-x-x-x-x-x-x-", "src\\main\\resources\\audio\\toms.aiff")
+    playBeats("x-----x-------", "src\\main\\resources\\audio\\crash_cymbal.aiff")
+}
+
+fun playBeats (beats: String, file: String) {
+    val parts = beats.split("x");
+    var count = 0
+    for (part in parts) {
+        count += part.length + 1
+        if (part == "") {
+            playSound(file)
+        } else {
+            Thread.sleep(100 * (part.length + 1L))
+            if (count < beats.length) {
+                playSound(file)
+            }
+        }
+    }
+}
+
+fun playSound(file: String) {
+    val clip = AudioSystem.getClip()
+    val audioInputStream = AudioSystem.getAudioInputStream(File(file))
+    clip.open(audioInputStream)
+    clip.start()
+}
